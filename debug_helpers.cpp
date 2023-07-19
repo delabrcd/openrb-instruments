1e47585630e7869c9b6417a16029d646bc9fdcb1
delabrcd <36674601+delabrcd@users.noreply.github.com>
2023-07-18 22:18:48 -0400

    WIP: move debug strings to progmem, use circ buf for output packets, add ~40ms delay

--- debug_helpers.cpp
+++ debug_helpers.cpp
@@ -3,37 +3,58 @@
 #ifdef SERIAL_DEBUG
 #include "adapter_structs.h"
 #include "HardwareSerial.h"
+#include "Usb.h"
 
-const char *getCommandName(int cmd) {
+#ifdef SERIAL_DEBUG
+#define debug(msg) Notify(PSTR(msg), 0x80);
+#else
+#define debug(msg) (void)
+#endif
+
+const char *printCommandName(int cmd) {
     switch (cmd) {
         case CMD_ACKNOWLEDGE:
-            return "ACK";
+            debug("ACK");
+            break;
         case CMD_ANNOUNCE:
-            return "ANNOUNCE";
+            debug("ANNOUNCE");
+            break;
         case CMD_STATUS:
-            return "CMD_STATUS";
+            debug("CMD_STATUS");
+            break;
         case CMD_IDENTIFY:
-            return "CMD_IDENTIFY";
+            debug("CMD_IDENTIFY");
+            break;
         case CMD_POWER_MODE:
-            return "CMD_POWER_MODE";
+            debug("CMD_POWER_MODE");
+            break;
         case CMD_AUTHENTICATE:
-            return "CMD_AUTHENTICATE";
+            debug("CMD_AUTHENTICATE");
+            break;
         case CMD_GUIDE_BTN:
-            return "CMD_GUIDE_BTN";
+            debug("CMD_GUIDE_BTN");
+            break;
         case CMD_AUDIO_CONFIG:
-            return "CMD_AUDIO_CONFIG";
+            debug("CMD_AUDIO_CONFIG");
+            break;
         case CMD_RUMBLE:
-            return "CMD_RUMBLE";
+            debug("CMD_RUMBLE");
+            break;
         case CMD_LED_MODE:
-            return "CMD_LED_MODE";
+            debug("CMD_LED_MODE");
+            break;
         case CMD_SERIAL_NUM:
-            return "CMD_SERIAL_NUM";
+            debug("CMD_SERIAL_NUM");
+            break;
         case CMD_INPUT:
-            return "CMD_INPUT";
+            debug("CMD_INPUT");
+            break;
         case CMD_AUDIO_SAMPLES:
-            return "CMD_AUDIO_SAMPLES";
+            debug("CMD_AUDIO_SAMPLES");
+            break;
         default:
-            return "Unknown CMD";
+            debug("Unknown CMD");
+            break;
     }
 }
 
@@ -49,7 +70,7 @@ void serialPrintHex(const uint8_t *val, const uint8_t &nval) {
 
 void printPacket(const XBPACKET &packet, const char *descriptor) {
     SERIAL_DEBUG.print(descriptor);
-    SERIAL_DEBUG.print(getCommandName(packet.buf.frame.command));
+    printCommandName(packet.buf.frame.command);
     serialPrintHex(packet.buf.buffer, packet.header.length);
     SERIAL_DEBUG.print("\r\n");
     return;
--- packet_helpers.h
+++ packet_helpers.h
@@ -4,7 +4,8 @@
 
 output_t outputForNote(const uint8_t &note);
 uint8_t  getSequence();
-bool     fillPacket(const uint8_t *input, const uint8_t &length, XBPACKET *packet);
+bool     fillPacket(const uint8_t *input, const uint8_t &length, XBPACKET *packet,
+                    unsigned long triggered_time = 0);
 void fillInputPacketFromControllerData(const uint8_t *data, const uint8_t &ndata, XBPACKET *packet);
 void updateDrumStateWithDrumInput(const output_t &out, const uint8_t &state,
                                   DrumInputData *drum_input);
